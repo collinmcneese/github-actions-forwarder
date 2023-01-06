9ce4a0d2d690662ba79a134097689840fdfa4a18
Collin McNeese <collinmcneese@github.com>
2023-01-05 18:55:55 -0600

    rework to js action
    Signed-off-by: Collin McNeese <collinmcneese@github.com>

---
+++ index.js
@@ -0,0 +1,49 @@
+// index.js
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+const request = require('request');
+
+async function reflector({github, context, targetUrl}) {
+  // Validate that targetUrl is a valid URL
+  const URL = require('url').URL;
+
+  function validateUrl(urlString) {
+    try {
+      new URL(urlString); // eslint-disable-line no-new
+      return true;
+    } catch (err) {
+      throw new Error(`Invalid targetUrl: ${urlString} \n ${err}`);
+    }
+  }
+
+  validateUrl(targetUrl);
+
+  let options = {
+    url: targetUrl,
+    method: 'POST',
+    headers: {
+      'X-GitHub-Event': context.eventName,
+      'Content-Type': 'application/json',
+      'Content-Length': context.payload.length,
+    },
+    body: JSON.stringify(context.payload),
+  };
+
+  // Send the request
+  return new Promise((resolve, reject) => {
+    console.log(`Sending payload to ${targetUrl} with options: ${JSON.stringify(options.headers)}`);
+
+    request(options, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else if (response.statusCode < 200 || response.statusCode >= 300) {
+        reject(new Error(`Error sending payload to ${targetUrl}: ${response.statusCode} - ${response.statusMessage}`));
+      } else {
+        resolve(`Payload sent to ${targetUrl} \n response: ${response.statusCode} - ${response.statusMessage}`);
+      }
+    });
+  });
+};
+
+reflector({github, context: github.context, targetUrl: core.getInput('targetUrl')});
