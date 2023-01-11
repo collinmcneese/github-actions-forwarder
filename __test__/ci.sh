f5b94fa6aa924774560120aa4ee5189bdcc659c2
Collin McNeese <collinmcneese@github.com>
2023-01-11 12:19:38 -0600

    test exec refactor
    Signed-off-by: Collin McNeese <collinmcneese@github.com>

---
+++ __test__/ci.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# CI script to run NPM tests and capture the exit code if any tests fail
+
+# Wrapper function to run a command and capture the exit code
+function ci-run {
+  $@
+  local status=$?
+  if [ $status -ne 0 ]; then
+    echo "Error: $@"
+    exit $status
+  fi
+  return $status
+}
+
+cd "$(dirname "$0")/.."
+
+ci-run npx eslint --ignore-path .eslintignore .
+ci-run npx cspell *.js *.md
+ci-run npx markdownlint-cli -c markdownlint.yml *.md
+ci-run npx jest
